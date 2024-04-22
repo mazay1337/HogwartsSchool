799822f7ccf7b6e9ac16f66d11222f6dfff7b9d9
Танин <mazayztn@yandex.ru>
2024-04-22 13:58:43 +0300

    scripts have been written.

--- scripts421.sql
+++ scripts421.sql
@@ -3,33 +3,3 @@ alter table student add CONSTRAINT name_unique UNIQUE (name);
 alter table student alter COLUMN name SET NOT NULL;
 alter table student alter COLUMN age SET DEFAULT 20;
 alter table faculties add CONSTRAINT name_color_unique UNIQUE (name, color);
-
-
-
-create table person
-(
-    id        integer primary key,
-    name      text,
-    age       integer,
-    is_driver boolean,
-    car_id    integer not null references car (id)
-);
-
-
-
-create table car
-(
-    id    integer primary key,
-    make  text,
-    model text,
-    price integer
-);
-
-
-select student.name, student.age
-from student
-         inner join faculty on student.faculty_id = faculty.id
-
-select student.name, student.age
-from student
-         inner join avatar on student.id = avatar.id
